901f7f13ba1b5547475e6ab878b7d55fdbeadaaf
topcoder <topcoder.n@gmail.com>
2025-12-01 00:55:47 +0500

    feat: add initial setup

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from '@topcoder/pages/App'
+import '@topcoder/styles/index.scss'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '127.7.7.7',
+    port: 80,
+    open: true,
+  },
+})
