792acdec5b2ff17a037a1345fe3b9aa9c7cca0f3
topcoder <topcoder.n@gmail.com>
2025-12-01 01:50:50 +0500

    style: add tailwind.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@topcoder/pages/App'
+import '@topcoder/styles/tailwind.css'
 import '@topcoder/styles/index.scss'
 
 createRoot(document.getElementById('root')!).render(
